15197c19ccf311d0911a93655c6a2d3a07fabc99
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-06-21 22:11:11 +0900

    FEAT : 가로형 calendar 추가

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -3,17 +3,19 @@ import { View, Text, TextInput, TouchableOpacity, Image, FlatList, ScrollView, K
 import styled  from 'styled-components/native';
 // component
 import {DefaultText, DefaultBoldText} from '~/components';
+// lib
+import CalendarStrip from 'react-native-calendar-strip';
 // asset
 import {Icons} from '~/assets';
 
 const DiaryContainer = styled.View`
-  border: 1px solid red;
   height: 100%;
-  background-color: #fff;
+  background-color: #fff; 
 `;
+
 const DiaryTitleArea = styled.View`
   margin-top: 23px;
-  padding: 0 20px;
+  padding: 0px 20px;
 `;
 const DiaryTitle = styled(DefaultBoldText)`
   color: #FFBF6A;
@@ -22,13 +24,22 @@ const DiaryTitle = styled(DefaultBoldText)`
 const CalendarContainer = styled.View`
  height: 85px;
  background-color: #F1F1F1;
- padding: 0 20px;
+ padding: 10px 20px;
  margin-top: 26px;
+ position: relative;
 `;
+const Week = styled(DefaultText)`
+  color: #000;
+  font-size: 15px;
+  position: absolute;
+  z-index: 5;
+  top: 10px;
+  left: 20px;
+`;
+
 const DiaryContentsContainer = styled.View`
   padding: 30px 20px;
   padding-top: 28px;
-  border: 1px solid red;
   flex-grow: 1;
 `;
 
@@ -39,6 +50,17 @@ const DiaryContents = styled.View`
   background-color: #fff;
   border: 1px solid #C4C4C4;
   border-radius: 5px;
+`;
+
+const DiaryContentsTitle = styled(DefaultText)`
+  color: #4A4A4A;
+  font-size: 14px;
+ 
+`;
+const DiaryContentsQuestion = styled(DefaultText)`
+  font-size: 14px;
+  margin-top: 17px;
+  color: #4A4A4A;
 `;
 
 const WriteBtn = styled.TouchableOpacity`
@@ -65,12 +87,32 @@ const Diary = () => {
       <DiaryTitle>내 글 쓰기</DiaryTitle>
       </DiaryTitleArea>
       <CalendarContainer>
-        <DefaultText>달력 추가 영역</DefaultText>
+        {/* 달력 추가 영역 */}
+        {/* https://www.npmjs.com/package/react-native-calendar-strip */}
+        <Week>1주차</Week>
+        <CalendarStrip          
+          style={{ height: '100%', paddingTop:10, marginBottom:20, }}
+          innerStyle ={{  justifyContent:'center',}}
+          dayContainerStyle={{}}
+          calendarHeaderStyle={{display:'none'}}
+          scrollable={true}
+          showDayName={false}
+          dateNumberStyle={
+            {color: '#000', 
+            fontSize: 15,
+            fontWeight: '400',
+          }}
+          dateContainerStyle={{
+            backgroundColor:'#fff',
+          }}
+          iconStyle={{display:'none'}}
+        />
+        {/* // 달력 추가 영역 */}
       </CalendarContainer>
       <DiaryContentsContainer>
         <DiaryContents>
-          <Text>오늘의 문답</Text>
-          <Text>당신이 가장 좋아하는 장소는 어디인가요?</Text>
+          <DiaryContentsTitle>오늘의 문답</DiaryContentsTitle>
+          <DiaryContentsQuestion>당신이 가장 좋아하는 장소는 어디인가요?</DiaryContentsQuestion>
         </DiaryContents>
       </DiaryContentsContainer>
       <WriteBtn activeOpacity={0.7}>
