ce0b9d9bede5c584da093399b7227d96034fc4e3
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-09-06 01:08:16 +0900

    STYLE : 감정 억제기 퍼블리싱

--- src/components/TabNavigator.js
+++ src/components/TabNavigator.js
@@ -8,100 +8,112 @@ import {HeaderOption, FolderMenu} from '~/components';
 import {Main, FeelingCalendar, EmotionSuppressor, Diary} from '~/pages';
 
 // react-navigation
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {forModalPresentationIOS} from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={HeaderOption}>
-      {/* Main */}
-      <Tab.Screen
-        name="Main"
-        component={Main}
-        options={{
-          headerRight: () => <FolderMenu />,
-          tabBarIcon: ({focused}) => (
-            <Image
-              style={{
-                tintColor: focused ? '#8291E6' : null,
-                resizeMode: 'contain',
-                height: 31,
-                width: 31,
-              }}
-              source={Icons.Home}
-            />
-          ),
-          tabBarLabel: () => {
-            return null;
-          },
-        }}
-      />
-      {/* Calendar */}
-      <Tab.Screen
-        name="Calendar"
-        component={FeelingCalendar}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <Image
-              style={{
-                tintColor: focused ? '#8291E6' : null,
-                resizeMode: 'contain',
-                height: 31,
-                width: 31,
-              }}
-              source={Icons.Cal}
-            />
-          ),
-          tabBarLabel: () => {
-            return null;
-          },
-        }}
-      />
-      {/* Diary */}
-      <Tab.Screen
-        name="Diary"
-        component={Diary}
-        options={{
-          headerTitle: '이후애',
-          tabBarIcon: ({focused}) => (
-            <Image
-              style={{
-                tintColor: focused ? '#8291E6' : null,
-                resizeMode: 'contain',
-                height: 31,
-                width: 31,
-              }}
-              source={Icons.Edit}
-            />
-          ),
-          tabBarLabel: () => {
-            return null;
-          },
-        }}
-      />
-      {/* Emotion Suppressor */}
-      <Tab.Screen
-        name="Emotion Suppressor"
-        component={EmotionSuppressor}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <Image
-              style={{
-                tintColor: focused ? '#8291E6' : null,
-                resizeMode: 'contain',
-                height: 31,
-                width: 31,
-              }}
-              source={Icons.Message}
-            />
-          ),
-          tabBarLabel: () => {
-            return null;
-          },
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      <Tab.Navigator screenOptions={HeaderOption}>
+        {/* COMPONENT  Main */}
+        <Tab.Screen
+          name="Main"
+          component={Main}
+          options={{
+            headerRight: () => <FolderMenu />,
+            tabBarIcon: ({focused}) => (
+              <Image
+                style={{
+                  tintColor: focused ? '#8291E6' : null,
+                  resizeMode: 'contain',
+                  height: 31,
+                  width: 31,
+                }}
+                source={Icons.Home}
+              />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+        />
+        {/* COMPONENT  Calendar */}
+        <Tab.Screen
+          name="Calendar"
+          component={FeelingCalendar}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Image
+                style={{
+                  tintColor: focused ? '#8291E6' : null,
+                  resizeMode: 'contain',
+                  height: 31,
+                  width: 31,
+                }}
+                source={Icons.Cal}
+              />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+        />
+        {/* COMPONENT  Diary */}
+        <Tab.Screen
+          name="Diary"
+          component={Diary}
+          options={{
+            headerTitle: '이후애',
+            tabBarIcon: ({focused}) => (
+              <Image
+                style={{
+                  tintColor: focused ? '#8291E6' : null,
+                  resizeMode: 'contain',
+                  height: 31,
+                  width: 31,
+                }}
+                source={Icons.Edit}
+              />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+        />
+        {/* COMPONENT  Emotion Suppressor */}
+        <Tab.Screen
+          name="Emotion Suppressor"
+          component={EmotionSuppressor}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Image
+                style={{
+                  tintColor: focused ? '#8291E6' : null,
+                  resizeMode: 'contain',
+                  height: 31,
+                  width: 31,
+                }}
+                source={Icons.Message}
+              />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+        />
+      </Tab.Navigator>
+      {/* <Stack.Navigator>
+        <Stack.Screen
+          name="Emotion Suppressor"
+          component={EmotionSuppressor}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator> */}
+    </>
   );
 };
 
--- src/pages/EmotionSuppressor.js
+++ src/pages/EmotionSuppressor.js
@@ -8,10 +8,11 @@ import {
   FlatList,
   ScrollView,
   Keyboard,
+  Touchable,
 } from 'react-native';
 import styled from 'styled-components/native';
 // component
-import {DefaultText, ChatBubble} from '~/components';
+import {DefaultText, DefaultMediumText, ChatBubble} from '~/components';
 // util
 import {useDidMountEffect} from '~/utils';
 // asset
@@ -25,22 +26,64 @@ const EmotionSuppressorContainer = styled.View`
   height: 100%;
   padding-bottom: 80px;
   justify-content: space-between;
+  background-color: #f6f8fd;
+`;
+
+const EmotionSuppressorHeader = styled.View`
+  flex-direction: row;
+  height: 50px;
+  background: #fff;
+  padding: 0 16px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const EmotionSuppressorHeaderTitle = styled(DefaultMediumText)`
+  color: #4a4a4a;
+  font-size: 16px;
+`;
+
+const BackBtnArea = styled.View`
+  height: 100%;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 0;
+`;
+const BackBtnIcon = styled.Image`
+  width: 10px;
+  height: 14px;
 `;
 
 const DescriptionArea = styled.View`
-  margin-top: 17px;
+  margin-top: 6px;
+  background-color: #fff;
+  padding: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
 `;
 
-const DescriptionText = styled(DefaultText)`
-  font-size: 10px;
-  text-align: center;
-  color: #9e9e9e;
+const ArrowIcon = styled.Image`
+  width: 14px;
+  height: 10px;
+  margin-bottom: 4px;
+`;
+
+const DescriptionText = styled(DefaultMediumText)`
+  font-size: 11px;
+  line-height: 16.5px;
+  text-align: left;
+  color: #a8a8a8;
 `;
 
 const ChatArea = styled.FlatList`
   flex-basis: 100%;
   flex-shrink: 1;
-  padding: 0 20px 17px 20px;
+  padding: 0 30px 17px 23px;
   flex-direction: column;
 `;
 const ChatAreaBottomDummy = styled.View`
@@ -51,16 +94,18 @@ const ChatAreaBottomDummy = styled.View`
 const TextInputArea = styled.View`
   flex-direction: row;
   align-items: center;
-  padding: 7px 10px 7px 8px;
-  background-color: #cdcdcd;
+  padding: 11px 20px;
+  background-color: #d8e0f3;
   flex-shrink: 0;
+  height: 58px;
 `;
 
 const StyledTextInput = styled.TextInput`
   flex-grow: 1;
   height: 100%;
   background-color: #fff;
-  padding: 5px 10px;
+  border-radius: 100px;
+  padding: 0px 15px;
   flex-basis: 100%;
   flex-shrink: 1;
   min-height: 39px;
@@ -78,28 +123,44 @@ const SendBtnIcon = styled.Image`
   height: 27px;
 `;
 
-const DescriptionComponent = () => {
+// COMPONENT 설명
+const DescriptionComponent = props => {
   return (
-    <DescriptionArea>
-      <DescriptionText>
-        이 곳은 충동적인 감정을 억제하기 위한 곳입니다. {'\n'}
-        상대방에게 하고 싶은 말이 있다면, 이 곳에 써주세요. {'\n'}
-        하루가 지나면, 당신의 말들은 사라집니다.
-      </DescriptionText>
-    </DescriptionArea>
+    <TouchableOpacity activeOpacity={1} onPress={props.onPress}>
+      {props.isOpen ? (
+        <DescriptionArea>
+          <DescriptionText>
+            이 곳은 충동적인 감정을 억제하기 위한 곳입니다. {'\n'}
+            상대방에게 하고 싶은 말이 있다면, 이 곳에 써주세요. {'\n'}
+            하루가 지나면, 당신의 말들은 사라집니다.
+          </DescriptionText>
+          <ArrowIcon source={Icons.ArrowUp} />
+        </DescriptionArea>
+      ) : (
+        <DescriptionArea>
+          <DescriptionText>
+            이 곳은 충동적인 감정을 억제하기 위한 곳입니다.
+          </DescriptionText>
+          <ArrowIcon source={Icons.ArrowDown} />
+        </DescriptionArea>
+      )}
+    </TouchableOpacity>
   );
 };
 
 const EmotionSuppressor = () => {
-  // state
+  // PARAM state
   const [chatText, setChatText] = useState('');
   const [chatDataList, setChatDataList] = useRecoilState(message);
   const filteredMessageList = useRecoilValue(filteredMessage);
   useRecoilState(filteredMessage);
   const [hasNewChat, setHasNewChat] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // Description open / close
 
+  // PARAM ref
   const scrollRef = useRef();
-  // chatDataList 갱신 시마다 스크롤 하단으로 내리기
+
+  // FUNCTION chatDataList 갱신 시마다 스크롤 하단으로 내리기
   useDidMountEffect(() => {
     if (hasNewChat) {
       scrollRef.current.scrollToEnd();
@@ -107,8 +168,8 @@ const EmotionSuppressor = () => {
     }
   }, [hasNewChat]);
 
+  // FUNCTION 카톡 보내기
   const handleUpdateChat = msg => {
-    // 카톡 보내기
     const msgData = {
       id: 'msgId' + (chatDataList.length + 1),
       message: msg,
@@ -121,19 +182,41 @@ const EmotionSuppressor = () => {
 
   return (
     <EmotionSuppressorContainer>
+      {/* COMPONENT header */}
+      <EmotionSuppressorHeader>
+        <BackBtnArea>
+          <BackBtnIcon source={Icons.Back} />
+        </BackBtnArea>
+        <EmotionSuppressorHeaderTitle>감정 억제기</EmotionSuppressorHeaderTitle>
+      </EmotionSuppressorHeader>
+      {/* COMPONENT description */}
+      <DescriptionComponent
+        onPress={setIsOpen.bind(this, !isOpen)}
+        isOpen={isOpen}
+        style={{
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 2,
+          elevation: 8,
+        }}
+      />
+      {/* COMPONENT Chat area */}
       <ChatArea
         data={filteredMessageList}
         renderItem={({item}) => {
           return <ChatBubble msg={item.message} />;
         }}
         windowSize={2}
-        ListHeaderComponent={DescriptionComponent}
         ListFooterComponent={ChatAreaBottomDummy}
         ref={scrollRef}
-        onContentSizeChange={() => scrollRef.current.scrollToEnd()}
+        // onContentSizeChange={() => scrollRef.current.scrollToEnd()}
         onLayout={() => scrollRef.current.scrollToEnd()}></ChatArea>
       <TextInputArea style={{elevation: 3}}>
-        {/* 채팅 입력창 */}
+        {/* COMPONENT 채팅 입력창 */}
         <StyledTextInput
           multiline={true}
           onChange={event => {
